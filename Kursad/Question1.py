091a371e17d81f3b02c373c066c2123d706912a8
Kursad <122346253+KursaDSc@users.noreply.github.com>
2025-05-11 05:02:22 +0200

    odev

--- Kursad/Question1.py
+++ Kursad/Question1.py
@@ -1,1 +1,21 @@
 # Question1 - Kursad
+import os
+
+class Rectangle():
+    def __init__(self, width, heigth):
+        self.width = width
+        self.heigth = heigth
+
+    def area(self):
+        return self.width * self.heigth    
+    
+    def perimeter(self):
+        return 2*(self.width + self.heigth)
+    
+rect = Rectangle(5,7)
+
+os.system("cls")
+
+print("Dikdörtgenin alanı: {}\n".format(rect.area()))
+print("Dikdörtgenin çevresi: {}\n".format(rect.perimeter()))
+
