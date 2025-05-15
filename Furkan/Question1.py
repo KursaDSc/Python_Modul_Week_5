f80a753ce97753d2433dc43a6d1fa8bbc3cbea92
FurkanBilal <furkanbilalcubuk@gmail.com>
2025-05-15 12:47:22 +0200

    Update Question1.py

--- Furkan/Question1.py
+++ Furkan/Question1.py
@@ -1,1 +1,18 @@
 # Question1 - Furkan
+class Rectangle:
+    def __init__(self, width, height): #<-- "__init__" meselesini anlamadim
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+# ucgen sinifinin ornegini olustur
+rectangle = Rectangle(5, 7)
+#alani ve cevresini yazdir
+rectangle_area = rectangle.area()
+rectangle_perimeter = rectangle.perimeter()
+
+print(f"Area: {rectangle_area}, Perimeter: {rectangle_perimeter}")
