c066dc1dd2c031d114f1f33274ef7785271b9008
Ali Büyükgüral <alibuyukgural@gmail.com>
2025-05-16 11:43:54 +0200

    Update Question4.py

--- Ali/Question4.py
+++ Ali/Question4.py
@@ -1,1 +1,39 @@
 # Question4 - Ali
+class Arac:
+    def __init__(self, marka, model, yil):
+        self.marka= marka
+        self.model= model
+        self.yil= yil
+
+    def ozellikler(self):
+        print(f"Arac Markasi: {self.marka}, Model: {self.model}, Yil: {self.yil}")    
+
+class Suv(Arac):
+    def __init__(self, marka, model, yil, dort_cekis):
+        super().__init__(marka, model, yil)    
+        self.dort_cekis= dort_cekis
+
+    def ozellikler(self):
+        super().ozellikler()
+        print(f"Cekis ozelligi: {self.dort_cekis}")
+
+
+class SporCar(Arac):
+    def __init__(self, marka, model, yil, max_speed):
+        super().__init__(marka, model, yil) 
+        self.max_speed= max_speed       
+
+    def ozellikler(self):
+        super().ozellikler()
+        print(f"Maximum hiz: {self.max_speed}")    
+
+        
+arac=Arac("Bmw", "M5", 2020)     
+suv= Suv("Toyota", "Rav4", 2024, "4x4")
+spor= SporCar("Subaru", "Insight", 2018, 280)
+
+arac.ozellikler()
+suv.ozellikler()
+spor.ozellikler()
+
+
